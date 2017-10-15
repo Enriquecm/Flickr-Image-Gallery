9f4f541ddcfb782b31dd84b00847174ad503e63c
Enrique Melgarejo <enriquecmelgarejo@hotmail.com>
2017-10-14 21:40:03 -0300

    Initial commit

---
+++ Flickr Image Gallery/FeedViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Flickr Image Gallery
+//
+//  Created by Enrique Melgarejo on 13/10/17.
+//  Copyright © 2017 Choynowski. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
