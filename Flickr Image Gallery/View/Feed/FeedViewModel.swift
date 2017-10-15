8d10a847b4d7ed74609dc86450de86a6274f9e7d
Enrique Melgarejo <enriquecmelgarejo@hotmail.com>
2017-10-14 21:55:32 -0300

    Project organization

---
+++ Flickr Image Gallery/View/Feed/FeedViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FeedViewModel.swift
+//  Flickr Image Gallery
+//
+//  Created by Enrique Melgarejo on 14/10/17.
+//  Copyright © 2017 Choynowski. All rights reserved.
+//
+
+import Foundation
---
+++ Flickr Image Gallery/View/Settings/SettingsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewModel.swift
+//  Flickr Image Gallery
+//
+//  Created by Enrique Melgarejo on 14/10/17.
+//  Copyright © 2017 Choynowski. All rights reserved.
+//
+
+import Foundation
