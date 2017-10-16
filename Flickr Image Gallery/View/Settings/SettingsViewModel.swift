519d334aaf1b0231ecb892f9be9441dca5701922
Enrique Melgarejo <enriquecmelgarejo@hotmail.com>
2017-10-16 02:19:57 -0200

    Deleted unused information

--- Flickr Image Gallery/View/Settings/SettingsViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SettingsViewModel.swift
-//  Flickr Image Gallery
-//
-//  Created by Enrique Melgarejo on 14/10/17.
-//  Copyright © 2017 Choynowski. All rights reserved.
-//
-
-import Foundation
